d08f0bfb60b51fe801317515c72ebc7102afcbd2
ELIEL DOUNA <97183542+elielreich23@users.noreply.github.com>
2024-08-06 23:35:17 -0300

    forget passsword

--- src/app/auth/forgetPwd/page.jsx
+++ src/app/auth/forgetPwd/page.jsx
@@ -1,24 +1,25 @@
-import styles from '../forgetPWd/styles.module.scss';
+import styles from "./styles.module.scss";
 import "../../../styles/global.scss"
+import Link from "next/link";
 
 
-const ForgotPassword = () => {
+
+export default function ForgotPassword() {
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
-        <div className={styles.logo}>taskero</div>
-      </div>
-      <div className={styles.right}>
-        <form className={styles.form}>
-          <h2 className={styles.title}>Forgot Password</h2>
-          <p className={styles.subtitle}>Reclaim your account easily</p>
-          <label htmlFor="email" className={styles.label}>Email Address</label>
-          <input type="email" id="email" className={styles.input} placeholder="Email Address" />
-          <button type="submit" className={styles.resetButton}>Reset Password</button>
-        </form>
+      <div className={styles.logo}>taskero</div>
+      <form className={styles.form}>
+        <h2 className={styles.title}>Forgot Password</h2>
+        <p className={styles.subtitle}>Reclaim your account easily</p>
+        <label htmlFor="email" className={styles.label}>Email Address</label>
+        <input type="email" id="email" className={styles.input} placeholder="Email Address" />
+        <button type="submit" className={styles.resetButton}>Reset Password</button>
+      </form>
+      <div className={styles.authButtons}>
+
+        <a href="#" className={styles.login}>Login</a>
+        <a href="#" className={styles.signup}>Sign Up</a>
       </div>
     </div>
   );
-};
-
-export default ForgotPassword;
+}
