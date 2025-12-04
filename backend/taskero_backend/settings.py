819874a84dffa9cc804ebba022abbe5f596aaa4d
ELIEL DOUNA <97183542+elielreich23@users.noreply.github.com>
2025-12-04 13:37:41 -0300

    setting fix

--- backend/taskero_backend/settings.py
+++ backend/taskero_backend/settings.py
@@ -2,6 +2,7 @@
 Django settings for taskero_backend project.
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -19,6 +20,7 @@ SECRET_KEY = "django-insecure-your-secret-key-here"
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
